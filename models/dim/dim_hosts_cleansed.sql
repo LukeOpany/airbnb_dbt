08d3aa82c4d127ff969baa59bd923f22c732db06
Luke Opany <brianopany@gmail.com>
2025-11-04 16:48:41 +0300

    Add dim_listings_cleansed and dim_hosts_cleansed models
    - Clean listings: parse price to numeric, validate minimum_nights
    - Clean hosts: handle null host names with 'anonymous' default

---
+++ models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,13 @@
+with src_hosts as (
+    select * from {{ ref('src_hosts') }}
+)
+select
+    host_id,
+    case
+      when host_name is null then 'anonymus'
+      else host_name
+    end as host_name,
+    is_superhost,
+    created_at,
+    updated_at
+from src_hosts
